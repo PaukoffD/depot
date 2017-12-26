b6f7ae01326be70f46c45c8c608bdfcf5615dfb3
dmitryR <paukov2967@gmail.com>
2017-12-26 15:15:13 +0300

    init

---
+++ spec/rails_helper.rb
@@ -0,0 +1,34 @@
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+# Prevent database truncation if the environment is production
+abort('The Rails environment is running in production mode!') if Rails.env.production?
+require 'spec_helper'
+require 'rspec/rails'
+require 'factory_girl'
+require 'database_cleaner'
+
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
+RSpec.configure do |config|
+  config.fixture_path = "#{::Rails.root}/spec/factiories"
+
+  config.use_transactional_fixtures = true
+  config.infer_spec_type_from_file_location!
+
+  config.filter_rails_from_backtrace!
+
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      # Choose a test framework:
+      with.test_framework :rspec
+
+      # Choose one or more libraries:
+      #with.library :active_record
+      #with.library :active_model
+      #with.library :action_controller
+      # Or, choose the following (which implies all of the above):
+      with.library :rails
+    end
+  end
+end
